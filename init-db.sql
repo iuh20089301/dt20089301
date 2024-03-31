33608979b42147ef63fb8b2b1bf43c5c8a59ddda
iuh20089301 <111863165+iuh20089301@users.noreply.github.com>
2024-03-31 09:32:44 +0700

    Update init-db.sql
    s

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH20089301 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', 'IUH20089301'),
+  ('Big Data', 'IUH20089301'),
+  ('Cloud Deployement', 'IUH20089301'),
+('Data Analysis', 'IUH20089301'),
+('Block Chain', 'IUH20089301');
